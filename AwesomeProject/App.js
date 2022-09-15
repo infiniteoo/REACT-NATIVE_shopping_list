68fb2b6282bbd84ea283207b08ab60995a65d36a
Troy Dorman <troydorman@gmail.com>
2022-09-15 03:11:03 -0600

    working out configuration errors.  major pain in the butt, but it worksnow

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Header from './components/Header';
+import uuid from 'react-native-uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid.v4(),
+      text: 'Milk',
+    },
+    {
+      id: uuid.v4(),
+      text: 'Eggs',
+    },
+    {
+      id: uuid.v4(),
+      text: 'Bread',
+    },
+    {
+      id: uuid.v4(),
+      text: 'Juice',
+    },
+  ]);
+
+  console.log(items);
+
   return (
     <View style={styles.container}>
       <Header />
