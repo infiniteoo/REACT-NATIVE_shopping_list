dcc88c7675e370f6ba64cd9c9f794cf06112f374
Troy Dorman <troydorman@gmail.com>
2022-09-15 03:57:28 -0600

    added error handling in the event item field is blank

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import uuid from 'react-native-uuid';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
@@ -32,9 +32,13 @@ const App = () => {
   };
 
   const addItem = text => {
-    setItems(prevItems => {
-      return [{id: uuid.v4(), text}, ...prevItems];
-    });
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', [{text: 'OK'}]);
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid.v4(), text}, ...prevItems];
+      });
+    }
   };
 
   return (
